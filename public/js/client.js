530c8c3fe4d012c43c01efe29802d0714c6939a4
Enrico Robazza <enricorobazzi@gmail.com>
2020-04-15 09:53:57 -0300

    Settings page

--- public/js/client.js
+++ public/js/client.js
@@ -82,6 +82,17 @@ TrelloPowerUp.initialize(
         },
       ];
     },
+    'show-settings': function (t, options) {
+      // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
+      // what should Trello show. We highly recommend the popup in this case as
+      // it is the least disruptive, and fits in well with the rest of Trello's UX
+      return t.popup({
+        title: 'Settings',
+        url: './settings.html',
+        height: 184, // we can always resize later, but if we know the size in advance, its good to tell Trello
+      });
+    },
+
     /*        
       
       🔑 Authorization Capabiltiies 🗝
